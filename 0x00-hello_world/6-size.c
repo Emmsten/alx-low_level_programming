f80fd8aa1362807e03743889340cbad04147313d
Emmsten <ofine360@gmail.com>
2023-02-16 18:17:58 -0800

    My first commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
-* A program that prints the size of various types on the computer it is supposed to run on	
+* main - A program that prints the size of various computer types
 * Return: 0 (success)
 */
 int main(void)
 {
- char a;
- int b;
- longint c;
- long ong int d;
- float f;
+char a;
+int b;
+long int c;
+long ong int d;
+float f;
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
