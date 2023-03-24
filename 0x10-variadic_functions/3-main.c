277207b2d1f52fec96876e6c1bc9386f932549e0
Emmsten <ofine360@gmail.com>
2023-03-23 20:31:48 -0700

    My update

---
+++ 0x10-variadic_functions/3-main.c
@@ -0,0 +1,16 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_all("ceis", 'B', 3, "stSchool");
+    return (0);
+}
+julien@ubuntu:~/0x0f. Variadic functions$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c 3-print_all.c -o d
+julien@ubuntu:~/0x0f. Variadic functions$ ./d 
+B, 3, stSchool
+julien@ubuntu:~/0x0f. Variadic functions$ 
