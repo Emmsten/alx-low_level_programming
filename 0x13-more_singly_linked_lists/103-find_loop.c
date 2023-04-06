8ef15328e95dc511bd72e40dd51e41000b52bcf9
Emmsten <ofine360@gmail.com>
2023-04-05 20:25:33 -0700

    E choke

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* find_listint_loop - Finds the beginning of a loop in a linked list
+* @head: Pointer to the head of the list
+*
+* Return: The address of the node where the loop starts, or NULL if there is no loop
+*/
+listint_t *find_listint_loop(listint_t *head)
+{
+listint_t *slow = head, *fast = head;
+while (fast != NULL && fast->next != NULL)
+{
+slow = slow->next;
+fast = fast->next->next;
+if (slow == fast)
+break;
+}
+if (fast == NULL || fast->next == NULL)
+return (NULL);
+slow = head;
+while (slow != fast)
+{
+slow = slow->next;
+fast = fast->next;
+}
+return (slow);
+}
