080d31d70c5cdb0a2eb497b3414fd2bf8e370e06
Emmsten <ofine360@gmail.com>
2023-04-09 09:56:03 -0700

    my commit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 * set_bit - sets a bit at a given index to 1
@@ -7,11 +8,12 @@
 *
 * Return: 1 for success, -1 for failure
 */
-int set_bit(unsigned long int *n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index)
 {
 if (index >= sizeof(unsigned long int) * 8)
 {
 return (-1);
 }
-return ((n >> index) & 1);
+*n |= (1ul << index);
+return (1);
 }
